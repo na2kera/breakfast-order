eb17998c4852e55a5e713c0d334ff6333914affe
na2kera <nat3mitaro@gmail.com>
2024-12-16 23:54:26 +0900

    トップ

--- app/page.tsx
+++ app/page.tsx
@@ -21,28 +21,32 @@ export default function Home() {
 
   if (session) {
     return (
-      <div className="text-center mt-12">
-        <h1 className="text-2xl font-bold">
-          Signed in as {session?.user?.email}
-        </h1>
-        <button
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">
+            Signed in as {session?.user?.email}
+          </h1>
+          <button
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
       </div>
     );
   }
   return (
-    <div className="text-center mt-12">
-      <h1 className="text-2xl font-bold">Not signed in</h1>
-      <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={() => signIn("google", { callbackUrl: "/saveuser" })}
-      >
-        Sign in
-      </button>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">Not signed in</h1>
+        <button
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={() => signIn("google", { callbackUrl: "/saveuser" })}
+        >
+          Sign in
+        </button>
+      </div>
     </div>
   );
 }
