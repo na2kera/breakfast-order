fe16908b97899e0edc92a550b163d05788156fa8
na2kera <nat3mitaro@gmail.com>
2024-12-16 12:47:23 +0900

    ログイン

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+
+// GET と POST の両方のハンドラーを作成
+const handler = NextAuth(authOptions);
+export const GET = handler;
+export const POST = handler;
