0b93a001ee82b1e9db192ffa5d0423126d47caf1
na2kera <nat3mitaro@gmail.com>
2024-12-16 23:05:49 +0900

    同線

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,12 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Page = () => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <button className="px-4 py-2 m-2 text-white bg-blue-500 rounded hover:bg-blue-600">
+      <button
+        onClick={() => router.push("/order")}
+        className="px-4 py-2 m-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+      >
         注文する
       </button>
-      <button className="px-4 py-2 m-2 text-white bg-green-500 rounded hover:bg-green-600">
+      <button
+        onClick={() => router.push("/history")}
+        className="px-4 py-2 m-2 text-white bg-green-500 rounded hover:bg-green-600"
+      >
         履歴を見る
       </button>
     </div>
