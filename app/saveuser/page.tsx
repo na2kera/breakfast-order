25d2e5cc05c3f8bd8517909a7552b6f7ba52ae25
na2kera <nat3mitaro@gmail.com>
2024-12-16 15:51:49 +0900

    ユーザー処理

---
+++ app/saveuser/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useEffect } from "react";
+import { getSession } from "next-auth/react";
+import { supabase } from "../utils/supabase/server";
+import { useRouter } from "next/navigation";
+
+const SaveUser = () => {
+  const router = useRouter();
+  useEffect(() => {
+    const sendUserData = async () => {
+      const session = await getSession();
+      console.log(session);
+
+      const { data, error } = await supabase.from("users").upsert(
+        {
+          email: session?.user?.email,
+          name: session?.user?.name,
+        },
+        {
+          onConflict: "email",
+        }
+      );
+
+      console.log(data);
+      if (error) {
+        console.error(error);
+      } else {
+        router.push("/");
+      }
+    };
+    sendUserData();
+  }, []);
+  return <div>しばらくお待ちください</div>;
+};
+
+export default SaveUser;
